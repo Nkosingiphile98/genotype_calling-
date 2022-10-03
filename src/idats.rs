1fcb3d2f0539bf0415965138bca6e5202eb1ac26
Ishmael Sithole <eliasishmael234@gmail.com>
2022-10-03 13:28:47 +0200

    Extraction update

--- src/idats.rs
+++ src/idats.rs
@@ -6,7 +6,8 @@ fn main()
 {
 
     let  paths = vec!["/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790010",
-    "/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790011"];
+    "/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790011",
+"/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790012"];
 
     for index in 0..paths.capacity()
     {
@@ -29,6 +30,31 @@ fn main()
         }
     }
 
-   
+    //Accessing all vector components/directories in the vector
+    for x in 0..faxvec.capacity() {
+
+        //Taking the name of the files from directories/paths..save it 
+        //as string
+        let file_name = faxvec[x].file_stem().unwrap().to_string_lossy();
+
+        //Check if the name contains keyword "grn"
+        if file_name.contains("2019"){
+
+            //Open the file
+            let mut file = File::open(faxvec[x].as_path()).expect("Cant open file");
+
+            //Declare a variable to store file contents
+            let mut contents = String::new();
+
+            //Read the file and store content to contents
+            file.read_to_string(&mut contents).expect("Cant read file");
+
+            //Print contents
+            println!("Content: {}",contents);
+
+        }
+
+    }
+
     }
 }
