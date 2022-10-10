2e45f25844fc90fcebfbf7379da86fb1d68ec06f
Ishmael Sithole <eliasishmael234@gmail.com>
2022-10-10 10:58:39 +0200

    Extraction - single user input path

--- src/idats.rs
+++ src/idats.rs
@@ -1,43 +1,64 @@
-use std::fs::File; 
+use std::io;
+use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
 
-fn main() 
-{
 
-    let  paths = vec!["/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790010",
-    "/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790011",
-"/dataB/popdata/HapMap/h3a/H3Africa_HapMap_IDATs/201958790012"];
+fn main() {
 
-    for index in 0..paths.capacity()
-    {
 
-        let mut faxvec: Vec<std::path::PathBuf> = Vec::new();
+    let mut input = String::new();
 
-    for element in Path::new(paths[index]).read_dir().unwrap()
-     {
+//Declare dir_vec(vector) to store file directories
+    let mut faxvec: Vec<std::path::PathBuf> = Vec::new();
+
+    println!("Enter Path:");
+
+
+//Read input line and store it in 'input' variable on press enter
+        match io::stdin().read_line(&mut input) {
+
+            
+            Ok(_) => {
+              
+
+
+                 println!("Path: {}",input) ;
+
+            },
+//If the input returns an error print the error
+            Err(e) => println!("Something went wrong : {}", e)
+        }
+
+
+    
+    for element in std::path::Path::new(input.as_str().trim()).read_dir().unwrap() {
+
+        //save file directory to path variable
         let path = element.unwrap().path();
-        if let Some(extension) = path.extension() 
-        {
-            if extension == "idat"
-             {
 
+        //Take the extension of every file.
+        if let Some(extension) = path.extension() {
+
+            //Get and Change file extension to lower case because 
+            //sometimes the extension is in upper case
+            if extension.to_ascii_lowercase() == "idat" {
+                
                 //Push file directory of all files of idat format
                 faxvec.push(path);
-            
+               
 
             }
         }
-    }
+    
+}
 
     //Accessing all vector components/directories in the vector
     for x in 0..faxvec.capacity() {
 
-        //Taking the name of the files from directories/paths..save it 
-        //as string
+        //Taking the name of the files from directories/paths..save it as string
         let file_name = faxvec[x].file_stem().unwrap().to_string_lossy();
 
-        //Check if the name contains keyword "grn"
+        //Check if the name contains keyword "2019"
         if file_name.contains("2019"){
 
             //Open the file
@@ -56,5 +77,5 @@ fn main()
 
     }
 
-    }
+
 }
