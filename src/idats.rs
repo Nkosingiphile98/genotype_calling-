842e452f72da789553d881ebfba9e07134fb7bf3
Nkosingiphile98 <nkosingiphileprosperity@gmail.com>
2022-10-21 10:32:52 +0200

    Reading bytes from IDAT files

--- src/idats.rs
+++ src/idats.rs
@@ -1,124 +1,132 @@
+use std::ffi::OsStr;
+use std::fs;
 use std::io;
-use std::fs::File;
+use std::fs::{DirEntry, File, read_dir};
+use std::io::BufReader;
 use std::io::prelude::*;
-
-fn main() 
-{
-
-   
-let mut input = String::new();
-let mut string = "";
-
-//String to check the input(paths/command)
-  
-//Declare dir_vec(vector) to store file directories
-let mut faxvec: Vec<std::path::PathBuf> = Vec::new();
-let mut faxvec_counter=0;
+use std::ops::Add;
+use std::path::{Path, PathBuf};
 
 
-//Declare paths(vector) to store input paths
-let mut paths = vec![];
+fn main() {
 
-//Index for counting input paths
-let mut index=1;
+    
 
-println!("Enter Paths. When done: INPUT '*'");
+    let mut input = String::new();
+    let mut string = "";
 
-//Create infinite loop for inputting as many paths as you want
-loop 
-    {
+    //String to check the input(paths/command)
+    let mut input_checker = String::new();
+
+    //Declare dir_vec(vector) to store file directories
+    let mut dir_vec: Vec<std::path::PathBuf> = Vec::new();
+
+    //Declare dir_vec_counter to count directories pushed into dir_vec
+    let mut dir_vec_counter=0;
+
+    //Declare paths(vector) to store inputted paths
+    let mut paths = vec![];
+
+    //Index for counting inputted paths
+    let mut index=1;
+
+    println!("Enter Paths. When done: INPUT '*t'");
+
+
+    //Create infinite loop for inputting as many paths as you want
+        loop{
 
         println!("{}Enter Path{}:",string,index);
-    
-//Read input line and store it in 'input' variable on press enter
-        match io::stdin().read_line(&mut input) 
-        {
 
-//When the input is not returning errors:
-            Ok(_) => 
-            {
-              
-//Check if input contains '*' to break loop. The special
-// character '*' was chosen because file/folder names cannot contain the character.
-                if input.contains("*") 
-                {
+            //Read input line and store it in 'input' variable on press enter
+        match io::stdin().read_line(&mut input) {
 
+            //When the input is not returning errors:
+            Ok(_) => {
+
+                //Check if input contains '*' to break loop. The special character '*' was chosen because file/folder names cannot contain the character.
+                if input.contains("*") {
 
                     break;
                 }
                 else {
+
+                    //Else Push path into paths vector
                     paths.push(input.trim().to_string());
 
-        }
+                }
 
             },
 
-//If the input returns an error print the error
+            //If the input returns an error print the error
             Err(e) => println!("Something went wrong : {}", e)
         }
-//clear the input to avoid combined paths
-        input.clear();
+
+            //clear the input to avoid combined paths
+            input.clear();
             index=index+1;
-            string="Input '*' to End/"
+
+
+            string="Input '*t' to End/"
 
     }
 
 
+
+
+
+    //print paths and size of paths-vector
     println!("{:?} {}",paths,index-1);
 
-//loop as many times as the paths-vector capacity
+
+    //loop as many times as the paths-vector capacity
     for index in  0..index-1{
-        
-//Loop for every file found in the paths vector
-// .trim() to remove white space characters such as Enter/Tab/Space.
-    for element in std::path::Path::new(paths[index].as_str().trim()).read_dir().unwrap() 
-    {
 
-//save file directory to path variable
-        let path = element.unwrap().path();
+        //Loop for every file found in the paths vector
+    for element in std::path::Path::new(paths[index].as_str().trim()).read_dir().unwrap() { // .trim() to remove white space characters such as Enter/Tab/Space.
 
-//Take the extension of every file.
-        if let Some(extension) = path.extension() {
+        let path = element.unwrap().path();  //save file directory to path variable
 
-//Get and Change file extension to lower case because 
-//sometimes the extension is in upper case
-            if extension.to_ascii_lowercase() == "idat" {
-//Push file directory of all files of idat format
-                faxvec.push(path);
-                faxvec_counter=faxvec_counter+1;//count all entries
+        if let Some(extension) = path.extension() { //Take the extension of every file.
+
+            //Get and Change file extension to lower case because sometimes the extension is in upper case
+            if extension.to_ascii_lowercase() == "idat" {         //Check if it is "IDAT".
+
+                //Push file directory of all files of "IDAT" format into the dir_vec
+                dir_vec.push(path);
+                dir_vec_counter=dir_vec_counter+1; //count all entries
 
             }
         }
     }
 }
 
-//print all file directories
-    println!("Faxvec capacity: {}",faxvec.capacity());
+    //print all file directories
+    println!("dir_vec capacity: {}",dir_vec.capacity());
 
     //Accessing all vector components/directories in the vector
-    for x in 0..faxvec_counter {
+    for x in 0..dir_vec_counter {
 
-//Taking the name of the files from directories/paths..save it as string
-        let file_name = faxvec[x].file_stem().unwrap().to_string_lossy();
+       read_idat(dir_vec[x].as_path());
+    }
 
-//Check if the name contains keyword "2019"
-        if file_name.contains("2019"){
+    fn read_idat(path: &Path) -> io::Result<()>{
 
-//Open the file
-            let mut file = File::open(faxvec[x].as_path()).expect("Cant open file");
+        let f = File::open(path)?;
+        let mut reader = BufReader::new(f);
+        let mut buffer = Vec::new();
 
-//Declare a variable to store file contents
-            let mut contents = String::new();
+        // Read file into vector.
+        reader.read_to_end(&mut buffer)?;
 
-//Read the file and store content to contents
-            file.read_to_string(&mut contents).expect("Cant read file");
-
-//Print contents
-            println!("Content: {}",contents);
-
+        // Read.
+        for value in buffer {
+            println!("BYTE: {:?}", value);
         }
+        Ok(())
 
     }
 
 
 }
+
