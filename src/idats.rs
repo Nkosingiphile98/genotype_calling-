e21e2f056c703a9496831558605097d7ae0dca5a
Ishmael Sithole <eliasishmael234@gmail.com>
2022-11-09 17:37:09 +0200

    k_means clustering

--- src/idats.rs
+++ src/idats.rs
@@ -1,16 +1,23 @@
+
 extern crate base64;
 use std::io;
 use std::io::Read;
 use std::fs::File;
 use std::io::BufReader;
+
+// Import the linfa prelude and KMeans algorithm
+use linfa::prelude::*;
+use linfa_clustering::KMeans;
+// We'll build our dataset on our own using ndarray and rand
+use ndarray::prelude::*;
+//use ndarray::stack;
+use rand::prelude::*;
+// Import the plotters crate to create the scatter plot
 use plotters::prelude::*;
-
-
-
+use ndarray::arr2;
 
 fn main() ->io::Result<()> {
 
-    
 
     let mut input = String::new();
 
@@ -87,7 +94,7 @@ fn main() ->io::Result<()> {
     let mut  buffer3 = Vec::new();
     reader.read_to_end(&mut buffer3)?;
 
-    let _vec_capacity = buffer3;
+    let vec_capacity = buffer3;
     //***************************************************//
 
     //*******************create output bmp image************************//
@@ -99,7 +106,7 @@ fn main() ->io::Result<()> {
         .set_label_area_size(LabelAreaPosition::Bottom, 40.0)
         .set_label_area_size(LabelAreaPosition::Right, 40.0)
         .set_label_area_size(LabelAreaPosition::Top, 40.0)
-        .caption("Allelic intensity ratio scatter plot", ("sans-serif", 40.0))
+        .caption("Scatter Plot", ("sans-serif", 40.0))
         .build_cartesian_2d(0.0..2147483647.0, 0..2147483647)
         .unwrap();
 
@@ -138,86 +145,111 @@ fn main() ->io::Result<()> {
 
     //Create 2d vector
     let mut two_d_vector=vec![];
-    //        Col
     two_d_vector.push(green_data); //        ___________
-    two_d_vector.push(red_data);   //  Raw  |__|__|__|__|
-    // Green |__|__|__|__|
-    // Red   |__|__|__|__|
+    two_d_vector.push(red_data);//        Col
 
 
 
+    //let mut green_data_32bit=vec![];
+    //let mut red_data_32bit=vec![];
 
-    
+    let mut all_data=vec![];
 
-
-
-    loop {
-
+    for _x in 0..(vec_capacity.capacity()/4) {
 
         // access green and red intensities
-        let green_int: u32 = u32::from_be_bytes([two_d_vector[0][loop_counter],two_d_vector[0][loop_counter+1],two_d_vector[0][loop_counter+2],two_d_vector[0][loop_counter+3]]);
-        let red_int: u32 = u32::from_be_bytes([two_d_vector[1][loop_counter],two_d_vector[1][loop_counter+1],two_d_vector[1][loop_counter+2],two_d_vector[1][loop_counter+3]]);
+        let  green_int: u32 = u32::from_be_bytes([two_d_vector[0][loop_counter], two_d_vector[0][loop_counter + 1], two_d_vector[0][loop_counter + 2], two_d_vector[0][loop_counter + 3]]);
+        let  red_int: u32 = u32::from_be_bytes([two_d_vector[1][loop_counter], two_d_vector[1][loop_counter + 1], two_d_vector[1][loop_counter + 2], two_d_vector[1][loop_counter + 3]]);
 
-        let mut red=vec![];
-        let mut green:Vec<i32>=vec![];
+            all_data.push([(red_int/100000) as f64,(green_int/100000) as f64]);
 
-        println!("Green: {}",green_int);
-        println!("Red: {}",red_int);
+        println!("{}",green_int);
 
-        if red_int !=0 && red_int!=green_int && green_int!=0 {
-
-            
-
-                let ratio :f64 = red_int as f64/(red_int as f64+green_int as f64);
-
-
-
-                red.push(red_int.as_f64());
-                green.push(green_int as i32);
-
-                //[(red_int,green_int)]
-                let vec3: Vec<(f64, i32)>= red.iter().cloned().zip(green.iter().cloned()).collect();
-
-                //println!("{:?}",vec3);
-
-
-                red.clear();
-                green.clear();
-                if ratio<0.34 && ratio>=0.0{
-
-                    ctx.draw_series(
-                        vec3.iter().map(|point| Circle::new(*point,  4.0_f64, &GREEN)),
-                    ).unwrap();
-                }
-                if ratio<0.67 && ratio>0.33{
-
-                    println!("{}",ratio);
-
-                    ctx.draw_series(
-                        vec3.iter().map(|point| Circle::new(*point, 4.0_f64, &BLUE)),
-                    ).unwrap();
-                }
-
-
-
-                if ratio<=1.0 && ratio>0.66{
-
-                    ctx.draw_series(
-                        vec3.iter().map(|point| Circle::new(*point,  4.0_f64, &RED)),
-                    ).unwrap();
-                }
-            
-        }
 
         loop_counter=loop_counter+4;
-        
-
 
     }
 
+    println!("Storing data completed");
+
+    //Cluster Data
+
+    let boxed = all_data.into_boxed_slice();
+    let data = arr2(&boxed);
+    println!("Clustering data...");
+    let dataset = DatasetBase::from(data);
+    let rng = thread_rng(); // Random number generator
+    let n_clusters = 3;
+    let model = KMeans::params_with_rng(n_clusters, rng)
+        .max_n_iterations(2000)
+        .tolerance(1e-9)
+        .fit(&dataset)
+        .expect("Error while fitting KMeans to the dataset");
+
+    let dataset = model.predict(dataset);
+    println!("{:?}", dataset.records.shape());
+    println!("{:?}", dataset.targets.shape());
+
+
+    let root = BitMapBackend::new("Clustered Data.bmp", (600, 400)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let x_lim = 0.0..55500.0f64;
+    let y_lim = 0.0..55500.0f64;
+
+    let mut ctx = ChartBuilder::on(&root)
+        .set_label_area_size(LabelAreaPosition::Left, 40) // Put in some margins
+        .set_label_area_size(LabelAreaPosition::Right, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("KMeans Demo", ("sans-serif", 25)) // Set a caption and font
+        .build_cartesian_2d(x_lim, y_lim)
+        .expect("Couldn't build our ChartBuilder");
+
+    ctx.configure_mesh().draw().unwrap();
+    let root_area = ctx.plotting_area();
+
+    // check_array_for_plotting(&dataset.records);
+
+    for i in 0..dataset.records.shape()[0] {
+        let coordinates = dataset.records.slice(s![i, 0..2]);
+        println!("plot..");
+        let point = match dataset.targets[i] {
+            0 => Circle::new(
+                (coordinates[0], coordinates[1]),
+                3,
+                ShapeStyle::from(&RED).filled(),
+            ),
+            1 => Circle::new(
+                (coordinates[0], coordinates[1]),
+                3,
+                ShapeStyle::from(&GREEN).filled(),
+            ),
+
+            2 => Circle::new(
+
+
+                (coordinates[0], coordinates[1]),
+                3,
+                ShapeStyle::from(&BLUE).filled(),
+            ),
+// Making sure our pattern-matching is exhaustive
+            _ => Circle::new(
+                (coordinates[0], coordinates[1]),
+                3,
+                ShapeStyle::from(&BLACK).filled(),
+            ),
+        };
+        println!("plotting...");
+          root_area
+           .draw(&point)
+           .expect("An error occurred while drawing the point!");
+
+    }
+
+
+//End of clustering
+    Ok(())
 }
-
-
 
 
 
